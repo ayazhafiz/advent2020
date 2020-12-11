ce5e0b5d17cc40fa697a2d1abe9f45f1b7b627ee
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2020-12-11 14:23:35 -0600

    Day 11 Haskell

---
+++ day11/Main.hs
@@ -0,0 +1,97 @@
+import           Text.Parsec             hiding ( Empty )
+import           Text.Parsec.String             ( Parser )
+
+data Seat = Occupied | Empty | Floor
+  deriving Eq
+
+instance Show Seat where
+  show Occupied = "#"
+  show Empty    = "L"
+  show Floor    = "."
+
+parseSeat :: Parser Seat
+parseSeat =
+  (string "#" >> return Occupied)
+    <|> (string "L" >> return Empty)
+    <|> (string "." >> return Floor)
+
+parseSeatRow :: Parser [Seat]
+parseSeatRow = many parseSeat
+
+nOccupied :: Seat -> Int
+nOccupied Occupied = 1
+nOccupied _        = 0
+
+countOccupied :: [[Seat]] -> Int
+countOccupied seats =
+  let rowCounts = map (foldl (flip ((+) . nOccupied)) 0) seats in sum rowCounts
+
+inBounds :: [[a]] -> Int -> Int -> Bool
+inBounds list row col =
+  row >= 0 && col >= 0 && row < length list && col < length (head list)
+
+type OccupiedAround = [[Seat]] -> Int -> Int -> Int
+type MapSeat = Int -> Seat -> Seat
+
+stepSeating :: OccupiedAround -> MapSeat -> [[Seat]] -> [[Seat]]
+stepSeating occupiedAround mapSeat seats = zipWith
+  (\r row -> zipWith (mapSeat . occupiedAround seats r) [0 ..] row)
+  [0 ..]
+  seats
+
+
+fix :: Eq a => (a -> a) -> a -> a
+fix f x = let x' = f x in if x == x' then x else fix f x'
+
+
+main = do
+  input <- lines <$> readFile "input.txt"
+  -- Using a list is a terrible idea, all indexing operations are O(n)... is
+  -- there a better way? Maybe a map?
+  let Right plane = mapM (parse parseSeatRow "") input
+
+  print "Part 1:"
+  let part1Occupied =
+        let occupiedAround :: OccupiedAround
+            occupiedAround seats r c =
+              let surrounding =
+                    [ (seats !! nr) !! nc
+                    | nr <- map (+ r) [-1 .. 1]
+                    , nc <- map (+ c) [-1 .. 1]
+                    , (nr, nc) /= (r, c)
+                    , inBounds seats nr nc
+                    ]
+              in  sum (map nOccupied surrounding)
+        in  let mapSeat :: MapSeat
+                mapSeat 0   Empty    = Occupied
+                mapSeat num Occupied = if num >= 4 then Empty else Occupied
+                mapSeat _   seat     = seat
+            in  let fixedPlane = fix (stepSeating occupiedAround mapSeat) plane
+                in  countOccupied fixedPlane
+  print part1Occupied
+
+  print "Part 2:"
+  let part2Occupied =
+        let occupiedAround :: OccupiedAround
+            occupiedAround seats r c =
+              let surrounding =
+                    [ map (\mul -> (r + mul * dr, c + mul * dc)) [1 ..]
+                    | dr <- [-1 .. 1]
+                    , dc <- [-1 .. 1]
+                    , (dr, dc) /= (0, 0)
+                    ]
+              in  let searchSeats :: [(Int, Int)] -> Int
+                      searchSeats ((r, c) : rest) = if not (inBounds seats r c)
+                        then 0
+                        else case (seats !! r) !! c of
+                          Empty    -> 0
+                          Occupied -> 1
+                          Floor    -> searchSeats rest
+                  in  sum (map searchSeats surrounding)
+        in  let mapSeat :: MapSeat
+                mapSeat 0   Empty    = Occupied
+                mapSeat num Occupied = if num >= 5 then Empty else Occupied
+                mapSeat _   seat     = seat
+            in  let fixedPlane = fix (stepSeating occupiedAround mapSeat) plane
+                in  countOccupied fixedPlane
+  print part2Occupied
